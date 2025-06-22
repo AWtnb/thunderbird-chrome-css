64ef29a609e649ad3bf38e7a431c5b8a7acbcd1b
awtnb <arikaebanataw@gmail.com>
2025-06-22 15:42:43 +0900

    update script

--- apply.ps1
+++ apply.ps1
@@ -1,2 +1,11 @@
-$dest = "$env:USERPROFILE\AppData\Roaming\Thunderbird\Profiles\" | Get-ChildItem | Sort-Object LastWriteTime | Select-Object -Last 1 | Get-ChildItem -Filter "chrome"
-Get-ChildItem "*.css" | ForEach-Object{$_ | Copy-Item -Destination $dest}
+$tbProfiles = "$env:USERPROFILE\AppData\Roaming\Thunderbird\Profiles"
+if (Test-Path $tbProfiles -PathType Container) {
+    $dest = $tbProfiles | Get-ChildItem | Sort-Object LastWriteTime | Select-Object -Last 1 | Get-ChildItem -Filter "chrome"
+    Get-ChildItem "*.css" | ForEach-Object{
+        "Copied '{0}' to profile." -f $_.Name | Write-Host -ForegroundColor Cyan
+        $_ | Copy-Item -Destination $dest
+    }
+}
+else {
+    "'{0}' not found!" -f $tbProfiles | Write-Host -ForegroundColor Magenta
+}
