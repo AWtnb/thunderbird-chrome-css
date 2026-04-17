950a2decfbc97821e3970e421a9ffce3a525bda9
AWtnb <46807238+AWtnb@users.noreply.github.com>
2026-04-17 09:45:56 +0900

    update

--- apply.ps1
+++ apply.ps1
@@ -1,10 +1,8 @@
 $tbProfiles = "$env:USERPROFILE\AppData\Roaming\Thunderbird\Profiles"
 if (Test-Path $tbProfiles -PathType Container) {
     $dest = $tbProfiles | Get-ChildItem | Sort-Object LastWriteTime | Select-Object -Last 1 | Get-ChildItem -Filter "chrome"
-    Get-ChildItem "*.css" | ForEach-Object{
-        "Copied '{0}' to profile." -f $_.Name | Write-Host -ForegroundColor Cyan
-        $_ | Copy-Item -Destination $dest
-    }
+    Get-ChildItem "*.css" | Copy-Item -Destination $dest
+    "Copied css files to {0}" -f $dest.FullName | Write-Host
 }
 else {
     "'{0}' not found!" -f $tbProfiles | Write-Host -ForegroundColor Magenta
